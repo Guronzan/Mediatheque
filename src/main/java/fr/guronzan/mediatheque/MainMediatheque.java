ef76deff3671c2a51d0f6acf131922fe83489a00
Guillaume Rodrigues <guillaume.rodrigues@gmail.com>
2013-08-27 14:27:40 +0200

    adding some hibernate mapping files for 1 table

--- src/main/java/fr/guronzan/mediatheque/MainMediatheque.java
+++ src/main/java/fr/guronzan/mediatheque/MainMediatheque.java
@@ -1,23 +1,29 @@
 package fr.guronzan.mediatheque;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 public class MainMediatheque {
 
 	private JFrame frame;
+	private final JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+	private final JPanel musicPanel = new JPanel();
+	private final JPanel moviesPanel = new JPanel();
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MainMediatheque window = new MainMediatheque();
+					final MainMediatheque window = new MainMediatheque();
 					window.frame.setVisible(true);
-				} catch (Exception e) {
+				} catch (final Exception e) {
 					e.printStackTrace();
 				}
 			}
@@ -36,8 +42,15 @@ public class MainMediatheque {
 	 */
 	private void initialize() {
 		this.frame = new JFrame();
-		this.frame.setBounds(100, 100, 450, 300);
+		this.frame.setBounds(100, 100, 1073, 580);
 		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		this.frame.getContentPane().add(this.tabbedPane, BorderLayout.NORTH);
+
+		this.tabbedPane.addTab("Musique", null, this.musicPanel, null);
+		this.tabbedPane.setEnabledAt(0, true);
+
+		this.tabbedPane.addTab("Vid\u00E9os", null, this.moviesPanel, null);
 	}
 
 }
