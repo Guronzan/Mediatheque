a7a3c8e1c9603693d29c7f8787085bf99ec2ce4d
Guillaume Rodrigues <guillaume.rodrigues@gmail.com>
2013-08-27 11:31:36 +0200

    initial adding of main class

---
+++ src/main/java/fr/guronzan/mediatheque/MainMediatheque.java
@@ -0,0 +1,43 @@
+package fr.guronzan.mediatheque;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class MainMediatheque {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainMediatheque window = new MainMediatheque();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainMediatheque() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		this.frame = new JFrame();
+		this.frame.setBounds(100, 100, 450, 300);
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
