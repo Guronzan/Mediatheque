e41a9c37623698cf3d48f94bb6dc5f1041edb899
Guillaume Rodrigues <guillaume.rodrigues@gmail.com>
2013-08-30 16:43:10 +0200

    adding entry point for webservice (only user part done)

---
+++ src/main/java/fr/guronzan/mediatheque/webservice/DBAccess.java
@@ -0,0 +1,58 @@
+package fr.guronzan.mediatheque.webservice;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import fr.guronzan.mediatheque.mappingclasses.dao.BookDao;
+import fr.guronzan.mediatheque.mappingclasses.dao.CDDao;
+import fr.guronzan.mediatheque.mappingclasses.dao.MovieDao;
+import fr.guronzan.mediatheque.mappingclasses.dao.UserDao;
+import fr.guronzan.mediatheque.mappingclasses.domain.User;
+
+@Service
+public class DBAccess {
+
+	@Resource
+	private UserDao userDao;
+
+	@Resource
+	private MovieDao movieDao;
+
+	@Resource
+	private CDDao cdDao;
+
+	@Resource
+	private BookDao bookDao;
+
+	public void addUser(final User user) {
+		this.userDao.create(user);
+	}
+
+	public void updateUser(final User user) {
+		this.userDao.createOrUpdate(user);
+	}
+
+	public void deleteUser(final User user) {
+		this.userDao.delete(user);
+	}
+
+	public User getUserFromID(final int id) {
+		return this.userDao.getUserById(id);
+	}
+
+	public User getUserFromFullName(final String name, final String forName) {
+		return this.userDao.getUserByFullName(name, forName);
+	}
+
+	public boolean checkPasswordFromID(final int userId, final String password) {
+		final User user = this.userDao.getUserById(userId);
+		return user.checkPassword(password);
+	}
+
+	public boolean checkPasswordFromFullName(final String name,
+			final String forName, final String password) {
+		final User user = this.userDao.getUserByFullName(name, forName);
+		return user.checkPassword(password);
+	}
+}
